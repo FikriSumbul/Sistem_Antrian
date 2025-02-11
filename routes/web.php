7a9deaca56ddf55b8545d3626e14645a939dcf76
Muhammad Fikri Maulana <mfikrimaulana131203@example.com>
2025-02-11 13:54:37 +0800

    Memperbaiki Route Pemanggilan Data Pasien

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,22 @@
 
 use App\Http\Controllers\DataPasienController;
 use App\Http\Controllers\PasienController;
+use App\Models\AntrianPasien;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/datapasien',[DataPasienController::class, 'dataPasien']);
-
-Route::get('/cari-pasien', function () {
+Route::get('/pasien', function () {
     return view('dataPasien');
 })->name('cari.pasien');
 
 Route::post('/cari-pasien', [PasienController::class, 'cariPasien'])->name('cari.pasien');
 
-Route::post('/reset-antrian', function () {
-    session()->forget('antrian');
-    return redirect()->back();
-})->name('reset.antrian');
+Route::post('/panggil/{nomor_rm}', [PasienController::class, 'panggilPasien'])->name('panggil.pasien');
+Route::post('/selesai/{nomor_rm}', [PasienController::class, 'selesaiPasien'])->name('selesai.pasien');
 
-Route::post('/panggil-pasien/{nomor_rm}', [PasienController::class, 'panggilPasien'])->name('panggil.pasien');
-Route::post('/selesai-pasien/{nomor_rm}', [PasienController::class, 'selesaiPasien'])->name('selesai.pasien');
+Route::post('/reset-antrian', [PasienController::class, 'resetAntrian'])->name('reset.antrian');
+
+Route::get('/data-antrian', [PasienController::class, 'tampilkanAntrian'])->name('data.antrian');
